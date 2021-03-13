0eabacc28e9db16bd9e5978b3341dddc95db7fe4
Ellmoe1 <74122706+Ellmoe1@users.noreply.github.com>
2021-03-13 21:10:49 +0900

    ユーザー管理機能の編集 (passwordの代入)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
+    password              {'hoge12'}
+    password_confirmation {'hoge12'}
     lastname              { '山田' }
     firstname             { '太郎' }
     lastnamekana          { 'ヤマダ' }
