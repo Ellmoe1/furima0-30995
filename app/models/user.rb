c59001345e51970bbe7b092cd1ffa830896ce1df
Ellmoe1 <74122706+Ellmoe1@users.noreply.github.com>
2021-03-13 16:06:56 +0900

    ユーザー管理機能 の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :nickname,            presence: true
-  validates :email,               presence: true
-  validates :password,            presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i } # 半角英数字
-  validates :lastname,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
-  validates :firstname,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
-  validates :lastnamekana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
-  validates :firstnamekana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
-  validates :birthdate,           presence: true
+  with_options presence: true do
+      validates :nickname
+      validates :email
+      validates :password,      length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i } # 半角英数字
+      validates :lastname,      format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+      validates :firstname,     format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+      validates :lastnamekana,  format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
+      validates :firstnamekana, format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
+      validates :birthdate
+  end
 end
